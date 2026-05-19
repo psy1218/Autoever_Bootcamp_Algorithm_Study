186e5b69e4f0c6cd961ecd1b6da5836634cb90e7
olehdnd <128152192+ltw30@users.noreply.github.com>
2026-05-20 04:28:56 +0900

    Update taewoong.py

--- week15/Ladder1/taewoong.py
+++ week15/Ladder1/taewoong.py
@@ -1,1 +1,20 @@
 
+T = 10
+for t in range(T):
+    test = int(input())
+    N = 100
+    ladder = [list(map(int, input().split())) for _ in range(N)]
+    d_y = N-1
+    d_x = ladder[N-1].index(2)
+    move = 0  #move=0:위로, move=1:왼쪽, move=2 오른쪽
+    while d_y > 0:
+        if (move == 0 or move == 1) and d_x > 0 and ladder[d_y][d_x-1]:#왼쪽으로 갈수잇고 왼쪽이 1이면 왼쪽으로
+            d_x -= 1
+            move = 1
+        elif (move == 0 or move == 2) and d_x < N-1 and ladder[d_y][d_x+1]:
+            d_x += 1
+            move = 2
+        else:
+            d_y -= 1
+            move = 0
+    print("#{} {}".format(test, d_x))
