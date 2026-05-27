fb1d73dc0fa43483d6b74aae09f02d34d464ca6f
olehdnd <128152192+ltw30@users.noreply.github.com>
2026-05-27 14:03:55 +0900

    Add bracket matching functionality in taewoong.py
    Implement bracket matching logic for multiple test cases.

--- week16/괄호 짝짓기/taewoong.py
+++ week16/괄호 짝짓기/taewoong.py
@@ -1,1 +1,27 @@
 
+for tc in range(10):
+    N = int(input())
+    a = list(map(str, input()))
+    arr = list()
+
+   
+    left = ['(', '{', '[', '<']
+    
+    right = [')', '}', ']', '>']
+    for i in range(N):
+        if a[i] in left:
+            arr.append(lst[i])
+        if a[i] in right:
+            
+            if right.index(a[i]) == left.index(arr[-1]):
+                
+                arr.pop()   
+            else:
+                break
+                
+    sol = 0
+    if len(arr) == 0:
+        sol = 1
+    
+    print("#{} {}".format(tc + 1, sol))
+
